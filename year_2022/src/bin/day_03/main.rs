ca8518e79b866bf034ed0c88c5cdb7b85360b037
Sebastian Lyng Johansen <seblyng98@gmail.com>
2022-12-03 21:03:35 +0100

    add day 3

--- year_2022/src/bin/day_03/main.rs
+++ year_2022/src/bin/day_03/main.rs
@@ -1,9 +1,67 @@
+use std::collections::HashSet;
+use std::str::FromStr;
+
+pub struct Rucksack {
+    first: String,
+    second: String,
+}
+
+impl FromStr for Rucksack {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (c1, c2) = s.split_at(s.len() / 2);
+        Ok(Rucksack {
+            first: c1.to_string(),
+            second: c2.to_string(),
+        })
+    }
+}
+
+pub trait RucksackItemScore {
+    fn rucksack_item_score(self) -> usize;
+}
+
+impl RucksackItemScore for char {
+    fn rucksack_item_score(self) -> usize {
+        if self.is_uppercase() {
+            self as usize - 38
+        } else {
+            self as usize - 96
+        }
+    }
+}
+
 fn task_one(input: &[String]) -> usize {
-    unimplemented!()
+    input.into_iter().fold(0, |acc, rucksack| {
+        let rucksack = Rucksack::from_str(rucksack).unwrap();
+        for c1 in rucksack.first.chars() {
+            for c2 in rucksack.second.chars() {
+                if c2 == c1 {
+                    return c1.rucksack_item_score() + acc;
+                }
+            }
+        }
+        acc
+    })
 }
 
 fn task_two(input: &[String]) -> usize {
-    unimplemented!()
+    input.chunks(3).into_iter().fold(0, |acc, group| {
+        let first: HashSet<char> = group.get(0).unwrap().chars().collect();
+        let second: HashSet<char> = group.get(1).unwrap().chars().collect();
+        let third: HashSet<char> = group.get(2).unwrap().chars().collect();
+
+        first
+            .intersection(&second)
+            .map(|c| c.to_owned())
+            .collect::<HashSet<char>>()
+            .intersection(&third)
+            .collect::<Vec<&char>>()
+            .pop()
+            .unwrap()
+            .rucksack_item_score()
+            + acc
+    })
 }
 
 fn main() {
