3f886facb75b515c7d71ea7c9a524496c8e89088
Sebastian Lyng Johansen <seblyng98@gmail.com>
2022-12-04 14:53:44 +0100

    add day 4

--- year_2022/src/bin/day_04/main.rs
+++ year_2022/src/bin/day_04/main.rs
@@ -1,9 +1,59 @@
+use std::{ops::RangeInclusive, str::FromStr};
+
+struct Pair {
+    first: RangeInclusive<i16>,
+    second: RangeInclusive<i16>,
+}
+
+impl FromStr for Pair {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (range1, range2) = s.split_once(',').unwrap();
+        let get_range = |s: &str| {
+            let (start, end) = s.split_once('-').unwrap();
+            RangeInclusive::new(start.parse().unwrap(), end.parse().unwrap())
+        };
+        Ok(Self {
+            first: get_range(range1),
+            second: get_range(range2),
+        })
+    }
+}
+
+trait Overlap {
+    fn overlap_all(&mut self) -> bool;
+    fn overlap_any(&mut self) -> bool;
+}
+
+impl Overlap for Pair {
+    fn overlap_all(&mut self) -> bool {
+        if self.first.len() < self.second.len() {
+            self.first.all(|x| self.second.contains(&x))
+        } else {
+            self.second.all(|x| self.first.contains(&x))
+        }
+    }
+    fn overlap_any(&mut self) -> bool {
+        if self.first.len() < self.second.len() {
+            self.first.any(|x| self.second.contains(&x))
+        } else {
+            self.second.any(|x| self.first.contains(&x))
+        }
+    }
+}
+
 fn task_one(input: &[String]) -> usize {
-    unimplemented!()
+    input
+        .into_iter()
+        .filter(|pair| Pair::from_str(pair).unwrap().overlap_all())
+        .count()
 }
 
 fn task_two(input: &[String]) -> usize {
-    unimplemented!()
+    input
+        .into_iter()
+        .filter(|pair| Pair::from_str(pair).unwrap().overlap_any())
+        .count()
 }
 
 fn main() {
