d81b5fd962fadf1759c6059ae49d6d4675d9ccc9
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2024-12-10 22:25:20 +0100

    Add day 8

--- 2024/day_08/src/main.rs
+++ 2024/day_08/src/main.rs
@@ -1,9 +1,122 @@
-fn task_one(input: &[String]) -> usize {
-    unimplemented!()
+use std::collections::{HashMap, HashSet};
+
+use matrix::Matrix;
+
+mod matrix;
+
+fn get_antinodes(a: (i32, i32), b: (i32, i32)) -> [(i32, i32); 2] {
+    let (x, y) = (a.0 - b.0, a.1 - b.1);
+    [(a.0 + x, a.1 + y), (b.0 - x, b.1 - y)]
 }
 
+fn task_one(input: &[String]) -> usize {
+    let matrix = Matrix::from(input);
+    let mut map: HashMap<&u8, Vec<(i32, i32)>> = HashMap::new();
+    for (pos, it) in matrix.iter() {
+        match it {
+            b'0'..=b'9' | b'a'..=b'z' | b'A'..=b'Z' => {
+                let pos = (pos.0 as i32, pos.1 as i32);
+                map.entry(it)
+                    .and_modify(|e| e.push(pos))
+                    .or_insert(vec![pos]);
+            }
+            _ => continue,
+        }
+    }
+
+    let mut set = HashSet::new();
+
+    for values in map.values() {
+        for pos1 in values.iter() {
+            for pos2 in values.iter() {
+                if pos1 == pos2 {
+                    continue;
+                }
+
+                let antinodes = get_antinodes(*pos1, *pos2);
+                if matrix.in_grid(antinodes[0]) {
+                    set.insert(antinodes[0]);
+                }
+
+                if matrix.in_grid(antinodes[1]) {
+                    set.insert(antinodes[1]);
+                }
+            }
+        }
+    }
+
+    set.len()
+}
+
+fn get_antinodes_2_1(
+    matrix: &Matrix<u8>,
+    set: &mut HashSet<(i32, i32)>,
+    a: (i32, i32),
+    b: (i32, i32),
+) {
+    if !matrix.in_grid(a) {
+        return;
+    }
+
+    set.insert(a);
+
+    let (x, y) = (a.0 - b.0, a.1 - b.1);
+    let new_a = (a.0 + x, a.1 + y);
+
+    get_antinodes_2_1(matrix, set, new_a, a);
+}
+
+fn get_antinodes_2_2(
+    matrix: &Matrix<u8>,
+    set: &mut HashSet<(i32, i32)>,
+    a: (i32, i32),
+    b: (i32, i32),
+) {
+    if !matrix.in_grid(b) {
+        return;
+    }
+
+    set.insert(b);
+
+    let (x, y) = (a.0 - b.0, a.1 - b.1);
+    let new_b = (b.0 - x, b.1 - y);
+
+    get_antinodes_2_2(matrix, set, b, new_b);
+}
+
+// Basically just need to keep adding antinodes on the map with the
+// same distance as between two frequencies, while it is still in the grid
 fn task_two(input: &[String]) -> usize {
-    unimplemented!()
+    let matrix = Matrix::from(input);
+    let mut map: HashMap<&u8, Vec<(i32, i32)>> = HashMap::new();
+    for (pos, it) in matrix.iter() {
+        match it {
+            b'0'..=b'9' | b'a'..=b'z' | b'A'..=b'Z' => {
+                let pos = (pos.0 as i32, pos.1 as i32);
+                map.entry(it)
+                    .and_modify(|e| e.push(pos))
+                    .or_insert(vec![pos]);
+            }
+            _ => continue,
+        }
+    }
+
+    let mut set = HashSet::new();
+
+    for values in map.values() {
+        for pos1 in values.iter() {
+            for pos2 in values.iter() {
+                if pos1 == pos2 {
+                    continue;
+                }
+
+                get_antinodes_2_1(&matrix, &mut set, *pos1, *pos2);
+                get_antinodes_2_2(&matrix, &mut set, *pos1, *pos2);
+            }
+        }
+    }
+
+    set.len()
 }
 
 fn main() {
---
+++ 2024/day_08/src/matrix.rs
@@ -0,0 +1,156 @@
+pub struct Matrix<T> {
+    vec: Vec<T>,
+    dim: (usize, usize),
+}
+
+pub struct MatrixIter<'a, T> {
+    matrix: &'a Matrix<T>,
+    curr: (i32, i32),
+}
+
+#[derive(Debug)]
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+    UpLeft,
+    UpRight,
+    DownLeft,
+    DownRight,
+}
+
+impl Direction {
+    pub fn to_index(&self, curr: (i32, i32)) -> (i32, i32) {
+        match self {
+            Direction::Up => (curr.0, curr.1 - 1),
+            Direction::Down => (curr.0, curr.1 + 1),
+            Direction::Left => (curr.0 - 1, curr.1),
+            Direction::Right => (curr.0 + 1, curr.1),
+            Direction::UpLeft => (curr.0 - 1, curr.1 - 1),
+            Direction::UpRight => (curr.0 + 1, curr.1 - 1),
+            Direction::DownLeft => (curr.0 - 1, curr.1 + 1),
+            Direction::DownRight => (curr.0 + 1, curr.1 + 1),
+        }
+    }
+}
+
+impl<T> Matrix<T> {
+    pub fn new(vec: Vec<T>, dim: (usize, usize)) -> Self {
+        Matrix { dim, vec }
+    }
+
+    pub fn iter(&self) -> MatrixIter<T> {
+        MatrixIter {
+            matrix: self,
+            curr: (-1, 0),
+        }
+    }
+
+    pub fn width(&self) -> usize {
+        self.dim.0
+    }
+
+    pub fn height(&self) -> usize {
+        self.dim.1
+    }
+
+    pub fn get(&self, pos: (i32, i32)) -> Option<&T> {
+        self.in_grid(pos).then(|| &self[pos])
+    }
+
+    pub fn iter_adjacent(&self, pos: (usize, usize)) -> impl Iterator<Item = (usize, usize)> + '_ {
+        (0..8).filter_map(move |i| {
+            let (x, y) = match i {
+                0 => (-1, -1),
+                1 => (-1, 0),
+                2 => (-1, 1),
+                3 => (0, 1),
+                4 => (1, 1),
+                5 => (1, 0),
+                6 => (1, -1),
+                7 => (0, -1),
+                _ => unreachable!(),
+            };
+            let (w, h) = (pos.0 as i32 + x, pos.1 as i32 + y);
+            self.in_grid((w, h)).then_some((w as usize, h as usize))
+        })
+    }
+
+    pub fn in_grid(&self, idx: (i32, i32)) -> bool {
+        idx.0 >= 0
+            && (idx.0 as usize) < self.height()
+            && idx.1 >= 0
+            && (idx.1 as usize) < self.width()
+    }
+}
+
+impl<'a, T> Iterator for MatrixIter<'a, T> {
+    type Item = ((usize, usize), &'a T);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let right = (self.curr.0 + 1, self.curr.1);
+        let down = (0, self.curr.1 + 1);
+
+        if self.matrix.in_grid(right) {
+            self.curr = right;
+            Some(((right.0 as usize, right.1 as usize), &self.matrix[right]))
+        } else if self.matrix.in_grid(down) {
+            self.curr = down;
+            Some(((down.0 as usize, down.1 as usize), &self.matrix[down]))
+        } else {
+            None
+        }
+    }
+}
+
+impl From<&[String]> for Matrix<u8> {
+    fn from(value: &[String]) -> Self {
+        Matrix::new(
+            value.iter().flat_map(|s| s.bytes()).collect(),
+            (value[0].len(), value.len()),
+        )
+    }
+}
+
+macro_rules! impl_index {
+    ($typ:ty) => {
+        impl<T> std::ops::Index<($typ, $typ)> for Matrix<T> {
+            type Output = T;
+            fn index(&self, idx: ($typ, $typ)) -> &T {
+                assert!(
+                    self.in_grid((idx.0 as i32, idx.1 as i32)),
+                    "index out of bounds: the dimensions are ({}, {}) but the index is ({}, {})",
+                    self.dim.0 - 1,
+                    self.dim.1 - 1,
+                    idx.0,
+                    idx.1
+                );
+                let idx = (idx.1 as usize * self.dim.0) + idx.0 as usize;
+                &self.vec[idx]
+            }
+        }
+    };
+}
+
+impl_index!(i32);
+impl_index!(usize);
+
+macro_rules! impl_fmt {
+    ($typ:ident,$lit:tt) => {
+        impl<T: std::fmt::$typ> std::fmt::$typ for Matrix<T> {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                for i in 0..self.vec.len() {
+                    if i % self.dim.0 == 0 {
+                        writeln!(f).unwrap();
+                    }
+                    write!(f, $lit, self.vec[i]).unwrap();
+                }
+                writeln!(f)
+            }
+        }
+    };
+}
+
+impl_fmt!(Debug, "{:?}, ");
+impl_fmt!(Display, "{}");
